2cb46df041fce089f8a3e2ef3dd34e9090e07c30
Blazerp <pranavseetha@gmail.com>
2016-05-15 15:52:43 -0700

    Pretty?

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 'use strict';
 
+
+
 function animateToTop(){
     $("containerRight").toggle();
   var elem = document.getElementById("containerTop");
   var rect = elem.getBoundingClientRect();
   var posy = rect.top;
- console.log(posy);
-  var id = setInterval(frame, .0001);
+  var id = setInterval(frame, .00001);
   function frame() {
           if(posy > 100){
           elem.style.top = posy-- ;
@@ -18,7 +19,6 @@ function animateToTopFB(){
   var elem = document.getElementById("containerBot");
   var rect = elem.getBoundingClientRect();
   var posy = rect.top;
- console.log(posy);
   var id = setInterval(frame, .0001);
   function frame() {
           if(posy > 90){
@@ -32,11 +32,14 @@ function animateToTopFR(){
   var rect = elem.getBoundingClientRect();
   var posy = rect.top;
   var posx = rect.left;
- console.log(posy);
+ console.log(posx);
   var id = setInterval(frame, .0001);
   function frame() {
-          if(posy > 90){
+          if(posy > 90  ){
           elem.style.top = posy-- ;
+      }
+      if(posx > 0){
+        elem.style.left = posx--;
       }
 }
 }
